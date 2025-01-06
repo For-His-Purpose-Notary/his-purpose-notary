f5cd0c8cf11ced31c274195fc736e9ed55b007f7
Davonne Vigil <davonnejv@gmail.com>
2025-01-06 01:24:31 -0600

    components not in components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-blue-300 text-4xl font-bold">His Purpose Notary</h1>
+      <Header />
     </>
   );
 }
