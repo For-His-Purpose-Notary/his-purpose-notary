50570b6f34c765a853b49c5bf2d3108493ddcb94
Davonne Vigil <davonnejv@gmail.com>
2025-01-10 16:47:17 -0600

    add more about section with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <main className="font-thin font-sans">
+      <main className="font-thin font-sans bg-neutral-100">
         <Home />
         <About />
       </main>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+import AboutMePics from "./AboutMePics";
+
 export default function About() {
   const aboutText = [
     `We are abitur pulvinar rutrum rhoncus. Donec eget risus sollicitudin, porttitor mi et, laoreet ante.
@@ -7,18 +9,21 @@ export default function About() {
       varius quam lobortis eu. Sed ex mauris, dignissim id massa ac, congue blandit nunc
      Morbi id nulla dapibus, gravida ante id, volutpat urna. Nam et elementum orci, non viverra diam`,
   ];
+
+  const whyNotary = [
+    `Morbi ut orci nibh. Curabitur blandit, massa ac dapibus pulvinar, sapien diam vestibulum justo, 
+     sit amet tincidunt lacus nibh sed tellus. Nulla consectetur eros non enim vestibulum, ac interdum 
+     eros hendrerit. Nullam aliquet nibh vitae`,
+  ];
   return (
-    <section className="flex flex-col justify-center items-center mt-20">
-      <h2 className="text-3xl lg:text-4xl p-8 flex mr-auto ml-20 lg:ml-48">
-        My Story
-      </h2>
-      <section className="max-w-xs md:max-w-xl lg:max-w-5xl flex flex-col gap-8 lg:flex-row">
+    <section className="flex flex-col justify-center items-center mt-10 lg:mt-20">
+      <section className="max-w-xs md:max-w-xl lg:max-w-5xl flex flex-col gap-8 ">
+        <h2 className="text-3xl lg:text-4xl">My Story</h2>
         <p className=" text-xl md:text-xl  lg:text-2xl">{aboutText}</p>
-        <img
-          src={" /images/placeholder.jpg"}
-          className="max-w-sm md:max-w-lg lg:max-w-lg"
-          alt="Image of Gina Vigil"
-        />
+
+        <h3 className="text-3xl lg:text-4xl">Why Notary?</h3>
+        <p className=" text-xl md:text-xl  lg:text-2xl">{whyNotary}</p>
+        <AboutMePics />
       </section>
     </section>
   );
