19f2b9a51b40b6e00cfcd954e19d8297872727cd
Davonne Vigil <davonnejv@gmail.com>
2025-01-19 18:05:17 -0600

    -

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center -mt-10">
       <section className="max-w-xs md:max-w-6xl min-h-screen text-center flex flex-col lg:flex-row justify-center items-center gap-8">
-        <section className="flex flex-col gap-4 mt-20 md:mt-0">
+        <section className="flex flex-col gap-4 mt-20 md:mt-40 lg:-mt-10">
           <h2 className="text-3xl md:text-5xl p-4 md:p-0">{ginasWhy}</h2>
           <p className="text-xl">{ourPurpose}</p>
           <p className="text-xl">{ourPurpose2}</p>
